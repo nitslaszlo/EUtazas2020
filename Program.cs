8124905a3c2e902bd9eece110da160cb1f132288
nitslaszlo <nitslaszlo@gmail.com>
2020-02-21 23:54:35 +0100

    up

--- Program.cs
+++ Program.cs
@@ -8,14 +8,13 @@ namespace EUtazas2020
         {
             Megoldás megoldás = new Megoldás("utasadat.txt");
 
-            Console.WriteLine($"2. feladat\nA buszra {megoldás.FelszállásokSzáma} utas akart felszállni.");
+            Console.WriteLine($"3. feladat\nA buszra {megoldás.FelszállásokSzáma} utas akart felszállni.");
 
-            Console.WriteLine($"3. feladat\nA buszra {megoldás.ÉrvénytelenFelszállásokSzáma} utas nem szállhatott fel.");
+            Console.WriteLine($"4. feladat\nA buszra {megoldás.ÉrvénytelenFelszállásokSzáma} utas nem szállhatott fel.");
 
-            Console.WriteLine($"4. feladat\nA legtöbb utas ({megoldás.MaxFelszálló} fő) a {megoldás.MaxMegálló}. megállóban próbált felszállni.");
+            Console.WriteLine($"5. feladat\nA legtöbb utas ({megoldás.MaxFelszálló} fő) a {megoldás.MaxMegálló}. megállóban próbált felszállni.");
 
-            Console.WriteLine("5. feladat");
-            Console.WriteLine($"Ingyenesen utazók száma: {megoldás.IngyenesUtazásDb} fő");
+            Console.WriteLine($"6. feladat\nIngyenesen utazók száma: {megoldás.IngyenesUtazásDb} fő");
             Console.WriteLine($"A kedvezményesen utazók száma: {megoldás.KedvezményesUtazásDb} fő");
 
             megoldás.Figyelmeztet("figyelmeztetes.txt");
